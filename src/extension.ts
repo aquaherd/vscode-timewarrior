81340d6abc5015ad74c6671d1f2ff01d8afa2f54
Andreas Weber <weber.andreas@gmail.com>
2026-03-04 21:07:48 +0100

    refactor: split commandsprovider and webview provider

--- src/extension.ts
+++ src/extension.ts
@@ -7,9 +7,13 @@ export function activate(context: vscode.ExtensionContext) {
   const dataFileEventEmitter = new vscode.EventEmitter<Array<DataFile>>();
 
   let dataFileWatcher = new DataFileWatcher(getConfig().get('basePath'), dataFileEventEmitter);
+
+  const monthSummaryProvider = new provider.MonthSummaryProvider(dataFileEventEmitter.event);
   context.subscriptions.push(
     ...[
-      new provider.CommandsProvider(dataFileEventEmitter.event),
+      new provider.CommandsProvider(),
+      monthSummaryProvider,
+      new provider.DayEditorProvider(monthSummaryProvider),
       new provider.DataFileTreeProvider(dataFileEventEmitter.event),
       new provider.StatusBarItemProvider(dataFileEventEmitter.event),
       new provider.ReminderProvider(dataFileEventEmitter.event),
